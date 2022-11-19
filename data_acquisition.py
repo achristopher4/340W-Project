61c5686db7abf18e6f85574af8e2b7ad87f458b4
achristopher4 <alexchristopher1999@yahoo.com>
2022-11-19 10:48:56 -0500

    Update changes to project. Removed large datasets for upload.

--- data_acquisition.py
+++ data_acquisition.py
@@ -1,15 +1,26 @@
 # Main file for collecting data that will be used in model
 
 import SteelDataset as sd
+import shutil
 
 train_num = 3000
 num_segmented = 50
 training_data = sd.STEELDataAcquisition('TRAIN', train_num, num_segmented)
-#testing_data = sd.STEELDataAcquisition('TEST')
 
-print(training_data)
-print(training_data.num_pos, training_data.num_neg)
-#print(training_data.data)
+train_pos_des = './STEEL_TRAINING/POSITIVE'
+train_neg_des = './STEEL_TRAINING/NEGATIVE'
+
+## Create positve samples train folder
+for pos_sample in training_data.training['pos']:
+    ## Copy and place into POSITIVE folder
+    src = pos_sample[4]
+    shutil.copy(src, train_pos_des)
+
+## Create negative smaples train folder
+for neg_sample in training_data.training['neg']:
+    ## Copy and place into NEGATIVE folder
+    src = neg_sample[4]
+    shutil.copy(src, train_neg_des)
 
 
 
